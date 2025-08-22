7b0aab6442471b045ac5b1da28b4946172fb2a93
Sanjai Kumar <youremail@example.com>
2025-08-22 12:01:14 +0530

    multiple next used and play in the route

--- src/app.js
+++ src/app.js
@@ -2,13 +2,33 @@ const express = require('express');
 
 const app = express();
 
-
-app.get("/user/:userID/:password/:name", (req, res) => {
-  console.log(req.query);
-  res.send({ firstName: "sanjai", lastName: "kumar" });
-});
-
-
+app.use(
+  "/user",
+  (req, res, next) => {
+    console.log("User middleware executed");
+    next();
+    //res.send("User route accessed");
+  },
+  (req, res, next) => {
+    console.error("Error in user route:");
+    //res.send("second response to one path 2");
+    next();
+  },[
+  (req, res, next) => {
+    console.error("Error in user route:");
+    //res.send("three response to one path 3");
+    next();
+  },
+  (req, res, next) => {
+    console.error("Error in user route:");
+    //res.send("four response to one path 3");
+    next();
+  }],
+  (req, res, next) => {
+    console.error("Error in user route:");
+    res.send("5th response to one path 5");
+  }
+);
 
 
 app.listen(3000, () =>{
