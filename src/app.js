b79cb4d6f1aba82d4313232081e16c1401669b86
Sanjai Kumar <youremail@example.com>
2025-08-23 13:29:44 +0530

    Dynamic data store in API

--- src/app.js
+++ src/app.js
@@ -3,15 +3,11 @@ const connectDB = require('./config/database');
 const app = express();
 const User = require("./models/user");
 
-
+app.use(express.json())
 app.post("/signup", async (req, res)=>{
   // creating a new instance of the User modal
-  const user = new User({
-    firstName: "sanjai",
-    lastName: "kumar",
-    emailId: "sanjai@kumargmail.com",
-    password: "sanjai@123"
-  });
+
+  const user = new User(req.body);
 try{
   await user.save();
   res.send("User registered successfully");
