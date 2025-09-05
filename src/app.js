0ba91bb86dbc2f0846ba0ce99607bac4fa9a40cb
Sanjai Kumar <youremail@example.com>
2025-09-05 18:45:39 +0530

    cors port changes

--- src/app.js
+++ src/app.js
@@ -1,42 +1,40 @@
-const express = require('express');
-const connectDB = require('./config/database');
+const express = require("express");
+const connectDB = require("./config/database");
 const app = express();
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
-    credentials: true
+    origin: [
+      "http://localhost:5173", // for local dev
+      "http://13.49.238.155", // your EC2 frontend
+    ],
+    credentials: true,
   })
 );
-app.use(express.json())
-app.use(cookieParser())
 
-const authRouter = require("./routes/auth")
-const profileRouter = require("./routes/profile")
-const requestRouter = require("./routes/request")
+app.use(express.json());
+app.use(cookieParser());
+
+const authRouter = require("./routes/auth");
+const profileRouter = require("./routes/profile");
+const requestRouter = require("./routes/request");
 const userRouter = require("./routes/users");
-
 
 app.use("/", authRouter);
 app.use("/", profileRouter);
-app.use("/", requestRouter)
+app.use("/", requestRouter);
 app.use("/", userRouter);
-
-
 
 connectDB()
   .then(() => {
     console.log("Database connected successfully");
 
-    app.listen(3000, () => {
+    app.listen(3000, "0.0.0.0", () => {
       console.log("Server is running on port 3000");
     });
   })
   .catch((err) => {
     console.error("Database connection failed:", err);
   });
-
-
-
