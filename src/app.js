082100cc3ad46b6e710d711a7f5f544d1778f897
Sanjai Kumar <youremail@example.com>
2025-09-18 14:57:17 +0530

    chat router build

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const connectDB = require("./config/database");
 const app = express();
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
+const http = require("http");
 
 require("dotenv").config();
 require("./utils/cronjobs");
@@ -17,23 +18,33 @@ app.use(
 app.use(express.json());
 app.use(cookieParser());
 
+
+
 const authRouter = require("./routes/auth");
 const profileRouter = require("./routes/profile");
 const requestRouter = require("./routes/request");
 const userRouter = require("./routes/users");
 const paymentRouter = require("./routes/payment");
+const initializeSocket = require("./utils/socket");
+const  chatRouter  = require("./routes/chat");
+
+// Create HTTP server for web sockets
+const server = http.createServer(app);
+initializeSocket(server);
+
 
 app.use("/", authRouter);
 app.use("/", profileRouter);
 app.use("/", paymentRouter);
 app.use("/", requestRouter);
 app.use("/", userRouter);
+app.use("/", chatRouter);
 
 connectDB()
   .then(() => {
     console.log("Database connected successfully");
 
-    app.listen(process.env.PORT,"0.0.0.0", () => {
+    server.listen(process.env.PORT,"0.0.0.0", () => {
       console.log("Server is running on port 3000");
     });
   })
